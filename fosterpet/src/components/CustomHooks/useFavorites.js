e2ed5ce1af3e72adb53f48564369054c7c401e9a
juanhuarte <juan_huarte@hotmail.com>
2022-06-02 00:08:10 -0300

    finding the mistake

--- fosterpet/src/components/CustomHooks/useFavorites.js
+++ fosterpet/src/components/CustomHooks/useFavorites.js
@@ -3,7 +3,11 @@ import { useSelector } from "react-redux";
 export const useFavorites = (id) => {
   const favorites = useSelector((state) => state.favorites);
   console.log("1", favorites);
-  if (favorites?.length > 0 && favorites?.find((animal) => animal.id === id))
+  if (
+    Array.isArray(favorites) &&
+    favorites?.length > 0 &&
+    favorites?.find((animal) => animal.id === id)
+  )
     return true;
   return false;
 };
